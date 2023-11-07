d29654ef613ac64ff7f90eb8c8d47ed866bdc6d7
vpm789 <vpm789@gmail.com>
2023-11-08 00:52:07 +0300

    check palindrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String s = "101";
+        System.out.println(CheckPalindrom(s));
+    }
 
+    public static boolean CheckPalindrom(String s) {
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arr[arr.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
